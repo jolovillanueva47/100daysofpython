e5368334c2fe78996cd0f9ca68ad551577a9edec
Jolo Villanueva <joselorenzovillanueva07@gmail.com>
2021-01-07 13:40:37 +0800

    Third day

---
+++ day_3/bmi_v2.py
@@ -0,0 +1,15 @@
+height=float(input("Enter your height in m: "))
+weight=int(input("Enter your weight in kg: "))
+bmi=weight/height**2
+bmi_rounded=round(bmi)
+print(bmi_rounded)
+if bmi_rounded <=18:
+    print(f"Your BMI is {bmi_rounded}, you are underweight")
+elif bmi_rounded <= 25:
+   print(f"Your BMI is {bmi_rounded}, you have a normal weight")
+elif bmi_rounded <= 30:
+    print(f"Your BMI is {bmi_rounded}, you are slightly overweight")
+elif bmi_rounded <= 35:
+    print(f"Your BMI is {bmi_rounded}, you are obese")
+else:
+    print(f"Your BMI is {bmi_rounded}, you are clinically obese")
---
+++ day_3/day3.py
@@ -0,0 +1,4 @@
+#Notes
+# One equal sign ("=") is an assignment operator
+# Two equal sign ("==") checks equality
+# A way to solve a coding problem is to create a flow chart first before coding to visualize the problem
