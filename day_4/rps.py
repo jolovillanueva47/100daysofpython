65c0d1599ce2902a571ead0deaf9a0cbb172fa01
Jolo Villanueva <joselorenzovillanueva07@gmail.com>
2021-01-09 13:10:49 +0800

    modified rps

--- day_4/rps.py
+++ day_4/rps.py
@@ -28,28 +28,32 @@ scissors = '''
 '''
 player_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
 choices=[rock,paper,scissors]
-you=choices[player_choice]
+
 computer_choice=random.randint(0,2)
 computer=choices[computer_choice]
-print(you)
-print("Computer chose: ")
-print(computer)
 
 player_wins="You Win"
 player_lose="You Lose"
 player_draw="It's a Draw"
 
-if player_choice == 0 and computer_choice == 1:
-    print(player_lose)
-elif player_choice == 0 and computer_choice == 2:
-    print(player_wins)
-elif player_choice == 1 and computer_choice == 0:
-    print(player_wins)
-elif player_choice == 1 and computer_choice == 2:
-    print(player_lose)
-elif player_choice == 2 and computer_choice == 0:
-    print(player_lose)
-elif player_choice == 2 and computer_choice == 1:
-    print(player_wins)
+if player_choice > 2 or player_choice < 0:
+    print("You entered an invalid number, you lose!")
 else:
-    print(player_draw)
+    you=choices[player_choice]
+    print(you)
+    print("Computer chose: ")
+    print(computer)
+    if player_choice == 0 and computer_choice == 1:
+        print(player_lose)
+    elif player_choice == 0 and computer_choice == 2:
+        print(player_wins)
+    elif player_choice == 1 and computer_choice == 0:
+        print(player_wins)
+    elif player_choice == 1 and computer_choice == 2:
+        print(player_lose)
+    elif player_choice == 2 and computer_choice == 0:
+        print(player_lose)
+    elif player_choice == 2 and computer_choice == 1:
+        print(player_wins)
+    else:
+        print(player_draw)
