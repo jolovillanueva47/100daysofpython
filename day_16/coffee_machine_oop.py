86afbd57b4fb4ca65617b9aedf15e7433145e8a1
Jolo Villanueva <joselorenzovillanueva07@gmail.com>
2021-01-26 13:55:11 +0800

    modified name of some variables

--- day_16/coffee_machine_oop.py
+++ day_16/coffee_machine_oop.py
@@ -2,23 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-coffeemaker=CoffeeMaker()
-moneymachine=MoneyMachine()
-coffeemenu=Menu()
+coffee_maker=CoffeeMaker()
+money_machine=MoneyMachine()
+coffee_menu=Menu()
 coffeemachine_is_on=True
 while coffeemachine_is_on:
-    choice=input("What would you like? "+coffeemenu.get_items()+":​ ")
+    choice=input("What would you like? "+coffee_menu.get_items()+":​ ")
     sufficient_resources=True
     if choice=="off":
          coffeemachine_is_on=False
     elif choice=="report":
-        coffeemaker.report()
-        moneymachine.report()
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink=coffeemenu.find_drink(choice)
+        drink=coffee_menu.find_drink(choice)
         if drink != None:
-            sufficient_resources=coffeemaker.is_resource_sufficient(drink)
+            sufficient_resources=coffee_maker.is_resource_sufficient(drink)
             if sufficient_resources == True:
-                moneymachine.make_payment(drink.cost)
-                coffeemaker.make_coffee(drink)
+                money_machine.make_payment(drink.cost)
+                coffee_maker.make_coffee(drink)
 
