bbdba14f930140d5602f48e1e718cd00de387a6c
Jolo Villanueva <el.gamer@yahoo.com.ph>
2021-06-10 16:23:22 +0800

    Finished Day 45 - there is an issue with the site being scraped, did some workaround for now

--- day_45/100_movies.py
+++ day_45/100_movies.py
@@ -4,5 +4,16 @@ import requests
 response=requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
 empire_webpage=response.text
 soup=BeautifulSoup(empire_webpage,"html.parser")
-movie_titles=soup.find(name="h3")
-print(movie_titles)
+
+movie_items=soup.select('.listicle-item')
+movie_list=[]
+for movie in movie_items:
+    movie_list.append(movie.img.get("alt"))
+movie_list.reverse()
+
+with open(file="movies.txt",mode="a") as file:
+    count=0
+    for movie in movie_list:
+        count+=1
+        file.write(f"{count}) {movie}\n")
+
