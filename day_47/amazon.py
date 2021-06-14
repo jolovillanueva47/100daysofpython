ff2ec3c6892a33939702ea49b99a33eaa78ea317
Jolo Villanueva <el.gamer@yahoo.com.ph>
2021-06-14 16:55:33 +0800

    trying some workarounds will continue tomorrow

--- day_47/amazon.py
+++ day_47/amazon.py
@@ -1,5 +1,7 @@
 import requests
+import lxml
 from bs4 import BeautifulSoup
+from amazoncaptcha import AmazonCaptcha
 
 
 amazon_url="https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/ref=sr_1_1?qid=1597662463"
@@ -12,5 +14,15 @@ amz_headers={
 }
 
 response=requests.get(amazon_url,headers=amz_headers)
+captcha_webpage=response.text
+soup=BeautifulSoup(captcha_webpage,"lxml")
+captcha_link=soup.select(".a-text-center>img")[0].get("src")
+print(captcha_link)
+captcha = AmazonCaptcha.fromlink(captcha_link)
+solution = captcha.solve()
+print(solution)
+# <div class="a-row a-text-center">
+# <img src="https://images-na.ssl-images-amazon.com/captcha/twhhswbk/Captcha_fwgzoazcal.jpg">
+# </div>
 
-print(response.text)
+#print(response.text)
